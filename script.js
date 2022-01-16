79f0c741b2cccfe6106b2b5f6a658583903ff588
Iggy36 <42382841+tayloriggy@users.noreply.github.com>
2022-01-15 22:48:18 -0600

    Created multiple functions to retrieve data from mealsDB and display to UI

--- script.js
+++ script.js
@@ -12,16 +12,68 @@ Steps:
 2. Create a button called "new recipe"
 3. When user clicks button "new recipe", display a new random recipe from the MealDB API endpoint/URL
 
-
+-create a specific function for each task i want to achieve, such as setting recipe title and displaying it to UI
+-call each function within newRandRec(data) 
+-call function newRandRec(data) at end to reset webpage for new rand rec with onclick event on "new recipe" button
+-use inner.HTML to push each key value to UI
+-define empty tag shells in html and set ID to each to grab id with getElementById
 
 
 */
 
-function newRandRec(){
+function randRec(){
 
-    axios.get("https://www.themealdb.com/api/json/v1/1/random.php/idMeal")
+    axios.get("https://www.themealdb.com/api/json/v1/1/random.php")
         .then(function (response) {
-                console.log('idMeal', response.data);
-        })
+            const data = response.data.meals[0];
+            setRecipeTitle(data);
+            setImage(data);
+            setIngredients(data);
+            setSteps(data);
+            setVideo(data);
+        });
 
+}
+
+function setRecipeTitle(recipe){
+    document.getElementById("title").innerHTML = recipe.strMeal;
+}
+
+function setImage(image){
+    document.getElementById("image").src = image.strMealThumb;
+}
+
+/*
+strIngredient1: "Water",
+  strIngredient10: "Basil",
+  strIngredient11: "Black Pepper",
+  strIngredient12: "",
+  strIngredient13: "",
+  strIngredient14: "",
+  strIngredient15: "",
+  strIngredient16: "",
+  strIngredient17: "",
+  strIngredient18: "",
+  strIngredient19: "",
+  strIngredient2: "Sugar",
+  strIngredient20: "",
+  strIngredient3: "Yeast",
+  strIngredient4: "Plain Flour",
+  strIngredient5: "Salt",
+  strIngredient6: "Olive Oil",
+  strIngredient7: "Passata",
+  strIngredient8: "Mozzarella",
+  strIngredient9:
+*/
+
+function setIngredients(ings){
+    document.getElementById("ingredients").innerHTML = ings.strIngredient1;
+}
+
+function setSteps(steps){
+    document.getElementById("instructions").innerHTML = steps.strInstructions;
+}
+
+function setVideo(vid){
+    document.getElementById("video").href = vid.strYoutube;
 }
